88f842f6bbc4d50dd2e6509010b74020b54b126e
Gurpreet Bajwa <gurpreetbajwa20@gmail.com>
2019-05-28 00:49:19 -0400

    Update actions.py
    multipart input to WS

--- actions.py
+++ actions.py
@@ -15,13 +15,19 @@ logger = logging.getLogger(__name__)
 class ActionJoke(Action):
     def name(self):
         # define the name of the action which can then be included in training stories
-        return "action_joke"
+        return "action_processfile"
 
     def run(self, dispatcher, tracker, domain):
         # what your action should do
-        request = json.loads(
-            requests.get("https://api.chucknorris.io/jokes/random").text
-        )  # make an api call
-        joke = request["value"]  # extract a joke from returned json response
-        dispatcher.utter_message(joke)  # send the message back to the user
+#        request = json.loads(
+#            requests.get("https://api.chucknorris.io/jokes/random").text
+#        )  # make an api call
+#        joke = request["value"]  # extract a joke from returned json response
+        filename = tracker.get_slot('filename')
+        file_prefix = "chatbot"
+        full_path = file_prefix + "/" + filename
+#       dispatcher.utter_message("The full path of you file is: " + full_path)  # send the message back to the user
+        with open('sample.xlsx', 'rb') as f:
+            r = requests.post('http://httpbin.org/post', files={'sample.xlsx': f})
+        dispatcher.utter_message(r.text.Host)
         return []
